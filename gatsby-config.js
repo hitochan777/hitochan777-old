7422ee1d36b4543fe8cd2f913ee3bab1fbfdcc66
Hitoshi Otsuki <hitochan777@gmail.com>
2019-03-09 08:12:51 +0000

    Migrate blog articles to _posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/src/pages/blog`,
+        path: `${__dirname}/_posts/blog`,
       },
     },
     {
